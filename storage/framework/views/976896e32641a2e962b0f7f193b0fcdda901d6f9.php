430847f6b30f5c9ed8ec20ddc0ef7a3556e90685
Trisha <trishacarvajal55@gmail.com>
2025-03-08 21:12:02 +0800

    new merge

---
+++ storage/framework/views/976896e32641a2e962b0f7f193b0fcdda901d6f9.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>VUE3/LARAVEL9 TEMPLATE</title>
+
+    <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?>
+
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html><?php /**PATH C:\Apache24\htdocs\vue3laravel9_template-master\vue3laravel9_template-master\resources\views/layouts/app.blade.php ENDPATH**/ ?>
